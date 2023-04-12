ef2ffbe8e0a0f32b352bc234033fcab2ab6b02be
sharifbekk <mustafozodashq888@gmail.com>
2023-04-12 15:48:21 +0500

    hometask

---
+++ HomeTask-day2.js
@@ -0,0 +1,95 @@
+
+
+//----------------------------------1-------------------------------
+
+
+
+// function calculatePower(voltage, current) {
+//     return voltage * current;
+// }
+// console.log(calculatePower(23, 10))
+
+
+
+
+//------------------------------------2---------------------------------
+
+
+
+
+// function DivisibleBy5(num) {
+
+//     if (num % 5 === 0) {
+//         return true;
+//     }
+
+//     else {
+//         return false;
+//     }
+// }
+// console.log(DivisibleBy5(25))
+
+
+
+//------------------------------------3---------------------------------
+
+// function equal(num1, num2) {
+//     if (num1 === num2) {
+//         return true
+//     }
+//     else return false
+// }
+// console.log(equal(5, 55))
+
+
+//-----------------------------------4----------------------------------
+
+
+// function longest(a, b, c) {
+//     h = a * 3600;
+//     m = b * 60;
+//     s = c;
+//     if (h > m && h > s) {
+//         return a;
+//     }
+//     else if (m > h && m > s) {
+//         return b;
+//     }
+//     else
+//         return c;
+// }
+// console.log(longest(1, 59, 3598))
+
+
+
+//--------------------------------5------------------------------------
+
+// function oddeven(num) {
+//     a = num % 10;
+//     b = Math.floor(num / 10)
+//     if ((a % 2 == 0 && b % 2 == 1) || (a % 2 == 1 && a % 2 == 0)) {
+//         return true;
+//     }
+//     else {
+//         return false;
+//     }
+// }
+// console.log(oddeven(12))
+
+
+//-------------------------------6---------------------------------------
+
+// function NegativeNum(num) {
+//     if (num > 0) {
+//         return -1 * num;
+//     }
+//     else if (num < 0) {
+//         return num;
+//     }
+//     else return 0;
+// }
+// console.log(NegativeNum(-5))
+
+
+
+//----------------------------------------------------------That's all-------------------------------------------
